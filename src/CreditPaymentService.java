264a68525a53bacf3113a9ba5f5267d46c0a90bb
Max test <max_dancer94@mail.ru>
2023-01-15 19:03:12 +0500

    Initial commit

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,14 @@
+public class CreditPaymentService {
+    public double calculate(int creditSum, double rate, int years) {
+        int month = years * 12;
+        double x = 100 * rate;
+        double ratePerMonth = rate / x;
+        int x5 = -month;
+        double x4 = 1 + ratePerMonth;
+        double x3 = Math.pow(x4, x5);
+        double x2 = 1 - x3;
+        double x1 = ratePerMonth / x2;
+        double amount = creditSum * x1;
+        return (int) amount;
+    }
+}
